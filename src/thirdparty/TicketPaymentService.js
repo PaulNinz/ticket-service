17cc8a1360cefc1327400fcaaa77a3e886010cd8
PaulNinz <leopaul.24@gmail.com>
2026-01-29 12:49:16 +0000

    Update TicketPaymentService.js

--- src/thirdparty/TicketPaymentService.js
+++ src/thirdparty/TicketPaymentService.js
@@ -1,8 +1,6 @@
 // Simulated external payment service
 export class TicketPaymentService {
   makePayment(accountId, totalAmountToPay) {
-    // Real implementation omitted; assume this charges a stored card.
-    // No return value needed.
 	console.log(
       `[PaymentService] Charging account ${accountId} £${totalAmountToPay}`
     );
