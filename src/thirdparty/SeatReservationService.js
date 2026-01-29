c8fb47121c399034d5f21932d101c8225cd9f28e
PaulNinz <leopaul.24@gmail.com>
2026-01-29 12:49:36 +0000

    Update SeatReservationService.js

--- src/thirdparty/SeatReservationService.js
+++ src/thirdparty/SeatReservationService.js
@@ -1,8 +1,6 @@
 // Simulated external seat reservation service
 export class SeatReservationService {
   reserveSeat(accountId, totalSeatsToAllocate) {
-    // Real implementation omitted; assume this reserves seats.
-    // No return value needed.
 	console.log(
       `[SeatService] Reserving ${totalSeatsToAllocate} seats for account ${accountId}`
     );
