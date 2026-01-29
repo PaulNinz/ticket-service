1a50a801d136c68fd21d16d0a2e9e1d32dea7475
Paul M <leopaul.24@gmail.com>
2026-01-29 13:35:28 +0000

    Update SeatReservationService.js

--- src/thirdparty/SeatReservationService.js
+++ src/thirdparty/SeatReservationService.js
@@ -1,6 +1,13 @@
 // Simulated external seat reservation service
 export class SeatReservationService {
   reserveSeat(accountId, totalSeatsToAllocate) {
+	  if (!Number.isInteger(accountId)) {
+      throw new TypeError('accountId must be an integer');
+    }
+
+    if (!Number.isInteger(totalSeatsToAllocate)) {
+      throw new TypeError('totalSeatsToAllocate must be an integer');
+    }
 	console.log(
       `[SeatService] Reserving ${totalSeatsToAllocate} seats for account ${accountId}`
     );
